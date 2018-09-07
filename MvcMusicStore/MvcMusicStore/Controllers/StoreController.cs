518840c522593a2eb45a237bccf2c11947befc07
sstarview <salmankhurshid.me@gmail.com>
2018-09-07 18:40:16 +0530

    Modified Store.Browse and Store.Details controller to take the query string of genre and id respectively

--- MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -6,12 +6,26 @@ using System.Web.Mvc;
 
 namespace MvcMusicStore.Controllers
 {
-    public class StoreController : Controller
+  public class StoreController : Controller
+  {
+    // GET: Store
+    public string Index()
     {
-        // GET: Store
-        public ActionResult Index()
-        {
-            return View();
-        }
+      return "Hello from Store.Index()";//View();
     }
+
+    //Get: Store/Browse?genre=Disco
+    public string Browse(string genre)
+    {
+      string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+      return message;
+    }
+
+    //Get: Store/Details/5
+    public string Details(int id)
+    {
+      string message = "Store.Details, Id = " + id;
+      return message;
+    }
+  }
 }
